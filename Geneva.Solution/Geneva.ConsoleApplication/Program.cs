33bf16deaeb6db9042a6fd3d7def4bf47839d323
RGD\leee <leee@reallygooddomain.com>
2020-07-22 22:26:47 +0100

    Code from last stream

--- Geneva.Solution/Geneva.ConsoleApplication/Program.cs
+++ Geneva.Solution/Geneva.ConsoleApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Recognition;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,44 @@ namespace Geneva.ConsoleApplication
 {
     class Program
     {
+        static readonly float confidenceLevel = 0.96f;
+
         static void Main(string[] args)
         {
+            var culture = new System.Globalization.CultureInfo("en-GB");
 
+            using (SpeechRecognitionEngine _speechRecognitionEngine = new SpeechRecognitionEngine(culture))
+            {
+                _speechRecognitionEngine.SpeechRecognized += _speechRecognitionEngine_SpeechRecognized;
+                _speechRecognitionEngine.SetInputToDefaultAudioDevice();
+
+                var choices = new Choices();
+                choices.Add("Geneva");
+                choices.Add("Jasper");
+
+                var grammarBuilder = new GrammarBuilder(choices);
+                grammarBuilder.Culture = culture;
+
+                var grammar = new Grammar(grammarBuilder);
+                _speechRecognitionEngine.LoadGrammar(grammar);
+
+                // Start asynchronous, continuous speech recognition
+                _speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+
+                // Keep the console window open
+                while (true)
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void _speechRecognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Confidence > confidenceLevel)
+            {
+                Console.WriteLine($"Speech Recognised= {e.Result.Text}; Confidence={e.Result.Confidence}");
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -6,8 +6,25 @@ A local voice controlled virtual assistant using C# and .NET
 * Activated by name (M)
 * Activation name should be configurable (S)
 * Have Passive & Active states (M)
+* Start in Passive state (M)
+* When hearing wake word, change to Active state (M)
+* After [a predefined time] in Active State, change to Passive State (M)
+* Use Voice Actors (Fivver) for predefined responses - less robotic (C)
+* Respond with voice (SpeechSynthesis) where applicable (M)
+* Allow voice to be Male or Female (M)
+
 
 ## Use Cases
 * Tell the time - "What time is it?" (M)
-* Tell the time in Japan - "What time is it in Japan"
+* Tell the time in Japan - "What time is it in Japan" (C)
+* Open Google
+* Google [blah]
 
+## To Do
+* Implement Configuration class
+* Encapsulate Geneva into separate classes (SRP etc)
+* Implement Passive and Active States
+* Implement Active state timeout
+* Implement Tell the time use case
+* Implement Male Voice by default
+* Investigate different Windows voice 'packs'
